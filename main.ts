8a2847967e7d6cebe7e5402d7a2546d789565c42
kukirobotclub <50830118+kukirobotclub@users.noreply.github.com>
2023-04-27 15:09:54 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 /*
  * PXT for KRC Microbit Control Bord
  * Copyright 2023 Bitcom K.Kurita
+ *                GNU
+ * 
+ * Version 2023-04-27 0.00
  * 
  * ブロック入出力を共通化して、入力と出力を結合すれば、ロボットが動く
+ * 入力											出力
+ * コントロールパッド━┓                    ┏━━モーター
+ * EEP-ROM   ━━━━━┫                    ┃
+ * BLE通信   ━━━━━╋━共通フォーマット━┻━━EEP-ROM
+ * シリアル入力  ━━━┫　mot_dat　opt_dat
+ * 赤外線リモコン受信━┛
+ * 
  */
